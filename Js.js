461b4579e5cac500e4defd66f2cb935e96bb466e
Elsayed4 <39974553+Elsayed4@users.noreply.github.com>
2021-09-16 09:04:09 +0200

    first commitss

--- Js.js
+++ Js.js
@@ -1,3 +1,4 @@
+/*
 if(document.readyState === "loading"){
     document.addEventListener('DOMContentLoaded',ready());
 } else {
@@ -22,8 +23,77 @@ function ready(){
         });
     });
 
+}
+*/
+
+let cards = Array.from(document.getElementsByClassName('card'));
+let cardBack = Array.from(document.getElementsByClassName('card-back'));
+let cardFront = Array.from(document.getElementsByClassName('card-front'));
+let flips = document.getElementById('flips');
+flipsNumber = Number(flips.innerHTML);
+console.log(cardBack)
+
+let flag = 0;
+let temp = 1;
+for(let x = 0; x< cards.length ;x+=2){
+    cards[x].name =(x/2);
+}
+
+for(let x = 0; x< cards.length ;x+=2){
+    cards[x+1].name =(x/2);
+}
 
 
 
+console.log()
+
+function resetAllCards(){
+
+    for(let r = 0 ; r<cards.length;r++){
+      cardBack[r].classList.remove("flip1");
+      cardFront[r].classList.remove("flip1");
+      
+      
+      console.log("remove")
+   }
 
 }
+
+    cards.forEach(card => {
+        card.addEventListener('click',() => {
+            //game.flipcard(card)
+            card.querySelector('.card-back').classList.add("flip1");
+            card.querySelector('.card-front').classList.add("flip2");
+           
+            if(flag % 2 == 0){
+                temp = card.name;
+            }else{
+                if(temp == card.name){
+                    setTimeout(function(){
+                        card.classList.add('removeCard');
+                        for(let c=0;c<cards.length;c++){
+                            if(cards[c].name == temp){
+                                cards[c].classList.add('removeCard');
+                            }
+
+                        }
+                        flips.innerHTML = --flipsNumber
+                    },1000)
+                    
+                    ;
+                }else{
+                    console.log("cry");
+                    //resetAllCards();
+                    for(let r = 0 ; r<cards.length;r++){
+                      setTimeout(function(){
+                        cardBack[r].classList.remove("flip1");
+                        cardFront[r].classList.remove("flip2");
+                      },1000)
+                     }
+                }
+            }
+            flag++;    
+        });
+    });
+
+    
